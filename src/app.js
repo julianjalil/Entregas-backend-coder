2a304013a1fc7a3d6a3af678bb719151ba2b482d
Julian Jalil <juliaanjalil@gmail.com>
2024-02-24 13:01:53 -0300

    desafioCuatro

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import express from "express";
-import productsRouter from "./routes/products.routes.js";
-import cartsRouter from "./routes/carts.routes.js";
+import { Server } from "socket.io";
+import handlebars  from "express-handlebars";
+import __dirname from "./utils.js";
+import router from "./routes/views.routes.js";
 
 const app = express();
-
 const PORT = 8080;
+const io = new Server(httpServer);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + "/views");
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"))
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.listen(PORT, () => {
+app.use('/', router)
+
+const httpServer = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
